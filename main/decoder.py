3607caba72e9bb5c93ca4be105d7f7b67d20a8b9
Victor <Victorsancheznogueira@gmail.com>
2023-11-29 00:22:02 +0100

    Refactoriced

--- main/decoder.py
+++ main/decoder.py
@@ -4,7 +4,6 @@ def decoder(data):
     data=separador_str_int(data)
     index=1
     dic={0:""}
-    string=""
     final_string=""
     for position,char in enumerate(data):
         # en el caso de que sea un str lo metera en el final string y al diccionario le añadira la nueva entry
@@ -16,3 +15,11 @@ def decoder(data):
         else:
             final_string+=dic[int(char)]
     return final_string
+if __name__ == "__main__":
+    def test_encoder():
+        assert decoder('0A0B1A2A4A4B')=='ABAABABAABAB'
+        assert decoder('0A0B1A2A4A4B3')=='ABAABABAABABAA'
+        assert decoder('0A0B2C3A2A4A6B6')=='ABBCBCABABCAABCAABBCAA'
+        assert decoder('0A1A2A3A4A')=='AAAAAAAAAAAAAAA'
+        assert decoder('0A0B0C1B3A2C4C7A6')=='ABCABCABCABCABCABC'
+        assert decoder('0A0B0C0D4E0F0G1B3D0E4B2D10A1E4A10D9A2C')=='ABCDDEFGABCDEDBBDEAAEDAEDCDABC'
